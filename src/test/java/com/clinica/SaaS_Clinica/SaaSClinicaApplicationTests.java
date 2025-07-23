7ff3f8fa13e74bd333c34aada27bde85f115f917
Erick Mendes <dev.erickmendes126@gmail.com>
2025-07-23 08:24:37 -0300

    Project start (Access to H2 database)

---
+++ src/test/java/com/clinica/SaaS_Clinica/SaaSClinicaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clinica.SaaS_Clinica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaaSClinicaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
